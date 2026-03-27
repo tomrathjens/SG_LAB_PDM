bf403bac8e59b4d7d4c1a3000baeaace7f7b845f
tomrathjens <tom.rathjens@epfl.ch>
2026-03-27 17:40:11 +0800

    fixed the rotation problem

--- controllers/controller.py
+++ controllers/controller.py
@@ -15,7 +15,7 @@ class Controller:
             self.left_wheel,
             p.VELOCITY_CONTROL,
             targetVelocity=-speed,
-            force =0.5
+            force =0.05
         )
 
         p.setJointMotorControl2(
@@ -23,21 +23,21 @@ class Controller:
             self.right_wheel,
             p.VELOCITY_CONTROL,
             targetVelocity=-speed,
-            force =0.5
+            force =0.05
         )
 
     def turn_left(self, speed=5):
 
         p.setJointMotorControl2(self.robot, self.left_wheel,
-                                p.VELOCITY_CONTROL, targetVelocity=speed, force =1)
+                                p.VELOCITY_CONTROL, targetVelocity=speed, force =0.05)
 
         p.setJointMotorControl2(self.robot, self.right_wheel,
-                                p.VELOCITY_CONTROL, targetVelocity=-speed, force =1)
+                                p.VELOCITY_CONTROL, targetVelocity=-speed, force =0.05)
         
     def turn_right(self, speed=5):
 
         p.setJointMotorControl2(self.robot, self.left_wheel,
-                                p.VELOCITY_CONTROL, targetVelocity=-speed, force =1)
+                                p.VELOCITY_CONTROL, targetVelocity=-speed, force =0.05)
 
         p.setJointMotorControl2(self.robot, self.right_wheel,
-                                p.VELOCITY_CONTROL, targetVelocity=+speed, force =1)
+                                p.VELOCITY_CONTROL, targetVelocity=+speed, force =0.05)
